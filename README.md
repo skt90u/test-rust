584dd5dcc9a5969bf4cc86541ac081780104ede7
skt90u <skt90u@gmail.com>
2023-07-27 23:51:45 +0800

    *

--- README.md
+++ README.md
@@ -5,3 +5,10 @@
 # 參考資料
 
 https://kaisery.github.io/trpl-zh-cn/ch02-00-guessing-game-tutorial.html
+
+## 開發模式 (watch)
+cargo watch -x run
+cargo watch -w src -x run
+
+Setting Up VSCode to Debug Rust
+https://www.youtube.com/watch?v=TlfGs7ExC0A
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("x 在內部範圍的數值為：{x}");
+    }
+
+    let mut spaces = "   ";
+    let mut a = spaces.len() + 1;
+    a+=1;
+    println!("x 的數值為：{a}");
 }
